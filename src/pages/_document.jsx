0868d9771a6099721eb79ac4de76e54e5243814e
Sukhbir Singh <Sukhbir@123_>
2023-08-18 17:31:52 +0530

    added script

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -27,6 +27,8 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6045708605795853"
+                    crossorigin="anonymous"></script>
             </body>
         </Html>
     )
