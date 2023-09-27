709236f325421fe946662fea5c778b734ff5d615
Sukhbir Singh <Sukhbir@123_>
2023-09-27 13:03:25 +0530

    updated favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,19 +5,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel='shortcut icon' href='/images/sukhfx.svg' />
-
-                {/* <meta property="og:title" content="Sukh - Professional Web &#38; Graphic Desinger" />
-                <meta property="og:description" content="Hey, I am Sukhbir Singh. A Front-end Developer/Graphic Designer from India who loves to design and code." />
-                <meta property="og:locale" content="en-IN" />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://sukhop.vercel.app/" />
-                <meta property="og:image" content="https://sukhop.vercel.app/images/web-preview.svg" />
-
-                <meta name="twitter:title" content="Sukh - Professional Web &#38; Graphic Desinger" />
-                <meta name="twitter:description" content="Hey, I am Sukhbir Singh. A Front-end Developer/Graphic Designer from India who loves to design and code." />
-                <meta name="twitter:image" content="https://sukhop.vercel.app/images/sukhfx.svg" />
-                <meta name="twitter:url" content="https://sukhop.vercel.app/" /> */}
+                <link rel='shortcut icon' href='/images/sukhop-favicon.svg' />
 
                 <meta name="description" content="Hey, I am Sukhbir Singh. A Front-end Developer/Graphic Designer from India who loves to design and code." />
                 <meta name="keywords" content="portfolio sukhfx, portfolio sukhbir, sukhfx skills Sukhop, Sukhbir singh, Sukhop, sukhfxgod" />
@@ -34,7 +22,7 @@ export default function Document() {
                 <meta property="twitter:url" content="https://sukhop.vercel.app" />
                 <meta name="twitter:title" content="Sukh - Professional Web & Graphic Desinger" />
                 <meta name="twitter:description" content="Hey, I am Sukhbir Singh. A Front-end Developer/Graphic Designer from India who loves to design and code." />
-                <meta name="twitter:image" content="https://sukhop.vercel.app/images/web-preview.png" />
+                <meta name="twitter:image" content="https://sukhop.vercel.app/images/web-preview.webp" />
 
                 <meta name="google-site-verification" content="shJXyPRlMaH-huGRnJEM7qcmrAN2sXrHT4eFHMi_r3I" />
 
